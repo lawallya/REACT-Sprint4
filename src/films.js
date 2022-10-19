61af61a4a7b1b9b05bfb2018482f7d5edde51dd9
Alejandra Cabello <alejandracabellomunoz@gmail.com>
2022-10-19 12:26:12 +0200

    ejercicio 5

--- src/films.js
+++ src/films.js
@@ -47,7 +47,17 @@ function orderAlphabetically(array) {
 
 // Exercise 5: Order by year, ascending
 function orderByYear() {
-
+//no son dos pasos uno detrás del otro, es en caso de que el año sea el mismo entonces ordena alfabéticamente    
+const peliculasOrdenadas = array.sort((a, b) => {
+  if (a.year > b.year) { return 1 }
+  if (a.year === b.year) {
+      let ac = (a.title > b.title) ? 1 : -1;
+      return ac;
+  }//estoy suponiendo que no hay 2 peliculas que se llamen igual
+  if (a.year < b.year) { return -1 }
+});
+console.table(peliculasOrdenadas);
+return peliculasOrdenadas;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
